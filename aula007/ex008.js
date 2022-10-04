d04be4b2b584411f0d1c66af8991cce02bcba9c1
FDavidPereira <111443145+FDavidPereira@users.noreply.github.com>
2022-10-04 13:20:20 -0300

    Interações com JS

---
+++ aula007/ex008.js
@@ -0,0 +1,6 @@
+var vel = 52.2
+console.log(`A velocidade do seu carro é ${vel}km/h `)
+if (vel > 60) {
+    console.log('Você ultrapassou a velocidade permitida. MUTADO!')
+}
+console.log('Ditija sempre usando cinto de segurança')
